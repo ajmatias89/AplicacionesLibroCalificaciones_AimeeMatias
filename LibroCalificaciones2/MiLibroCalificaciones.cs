658ad9e80ccee456993ea33498c84e222fa70d61
ajmatias89 <ajmatias89@gmail.com>
2026-08-30 19:57:44 -0500

    Validaciones

---
+++ LibroCalificaciones2/MiLibroCalificaciones.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibroCalificaciones2
+{
+    public class MiLibroCalificaciones
+    {
+        public void MostrarMensaje(string nombreCurso)
+        {
+            Console.WriteLine("¡Bienvenido al libro de calificaciones para: \n{0} !", nombreCurso);
+        }
+    }
+}
